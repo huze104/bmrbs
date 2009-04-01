f6965cf1afec5053744f4e186705409de9f7fda8
1982hongzhi <1982hongzhi@gmail.com>
2009-04-01 12:36:54 +0000

    测试数据

---
+++ document/sql/schema_mysql.sql
@@ -0,0 +1,205 @@
+drop database if exists bmrbs;
+create database bmrbs DEFAULT CHARACTER SET utf8;
+use bmrbs;
+set names 'utf8';
+
+
+
+/*==============================================================*/
+/* Table: mrbs_area                                             */
+/*==============================================================*/
+create table mrbs_area
+(
+   id                   int(11) not null auto_increment comment '主键',
+   area_name            varchar(30) default '' comment '名称',
+   linkman              varchar(255) comment '管理员联系方式',
+   descn                text default ' ',
+   shortdescn           text,
+   primary key (id)
+)
+comment = "区域"
+ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+
+/*==============================================================*/
+/* Table: mrbs_authorities                                      */
+/*==============================================================*/
+create table mrbs_authorities
+(
+   id                   integer not null auto_increment,
+   name                 varchar(64) not null,
+   display_name         varchar(64) not null,
+   primary key (id)
+)
+ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+
+/*==============================================================*/
+/* Table: mrbs_pod                                              */
+/*==============================================================*/
+create table mrbs_pod
+(
+   id                   integer not null auto_increment,
+   name                 varchar(255) not null,
+   component_class      varchar(255) not null,
+   primary key (id)
+)
+ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+
+/*==============================================================*/
+/* Table: mrbs_roles                                            */
+/*==============================================================*/
+create table mrbs_roles
+(
+   id                   integer not null auto_increment,
+   name                 varchar(64) not null,
+   primary key (id)
+)
+ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+
+/*==============================================================*/
+/* Table: mrbs_roles_authorities                                */
+/*==============================================================*/
+create table mrbs_roles_authorities
+(
+   role_id              integer not null,
+   authority_id         integer not null,
+   constraint FK_Reference_4 foreign key (role_id)
+      references mrbs_roles (id),
+   constraint FK_Reference_5 foreign key (authority_id)
+      references mrbs_authorities (id)
+)
+ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+
+/*==============================================================*/
+/* Table: mrbs_room                                             */
+/*==============================================================*/
+create table mrbs_room
+(
+   id                   int(11) not null auto_increment,
+   area_id              int(11) default NULL comment '区域',
+   room_name            varchar(255) default NULL comment '名称',
+   description          varchar(255) default NULL comment '简介',
+   capacity             int(11) default 0 comment '容纳人数',
+   room_admin_email     varchar(255) comment '联系方式',
+   status               varchar(32),
+   sortIndex            varchar(32),
+   virtualMap           varchar(255),
+   primary key (id),
+   constraint FK_Reference_1 foreign key (area_id)
+      references mrbs_area (id)
+)
+ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+
+/*==============================================================*/
+/* Table: mrbs_user                                             */
+/*==============================================================*/
+create table mrbs_user
+(
+   id                   int not null auto_increment comment '主键',
+   login_name           varchar(32) default ' ' comment '登陆名',
+   name                 varchar(32) comment '名字',
+   email                varchar(32) comment 'email',
+   tel                  varchar(32) comment '分机',
+   hi                   varchar(32) comment '百度hi',
+   department           varchar(64) comment '部门',
+   view_choice          varchar(32),
+   status               int default 0,
+   password             varchar(32),
+   assistant            int default 0,
+   primary key (id)
+)
+comment = "用户信息";
+
+/*==============================================================*/
+/* Table: mrbs_user_pod                                         */
+/*==============================================================*/
+create table mrbs_user_pod
+(
+   pod_id               integer not null,
+   id                   int comment '主键',
+   constraint FK_Reference_6 foreign key (id)
+      references mrbs_user (id),
+   constraint FK_Reference_7 foreign key (pod_id)
+      references mrbs_pod (id)
+)
+ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+
+/*==============================================================*/
+/* Table: mrbs_users_roles                                      */
+/*==============================================================*/
+create table mrbs_users_roles
+(
+   role_id              integer not null,
+   id                   int comment '主键',
+   constraint FK_Reference_2 foreign key (role_id)
+      references mrbs_roles (id),
+   constraint FK_Reference_3 foreign key (id)
+      references mrbs_user (id)
+)
+ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+
+/*==============================================================*/
+/* Table: mrbs_repeat                                           */
+/*==============================================================*/
+create table mrbs_repeat
+(
+   id                   int(11) not null auto_increment,
+   start_hour           varchar(2) not null comment '开始时间,以小时计',
+   end_hour             varchar(2) not null comment '结束时间',
+   start_mini           varchar(2) default '00' comment '开始分钟',
+   end_mini             varchar(2) not null default '00' comment '结束分钟',
+   room_id              int(11) not null comment '会议室',
+   updatetime           timestamp not null,
+   create_by            varchar(80) not null default '' comment '添加者',
+   orderman             varchar(32) not null default '' comment '预定者,存个名字而已',
+   rep_opt              int not null default 0 comment '重复情况',
+   description          text comment '会议室描述',
+   start_date           datetime default NULL comment '开始日期',
+   end_date             datetime comment '结束日期',
+   allday               int default 0 comment '是否整天',
+   repeat_week_day      int default 1 comment '重复星期',
+   week_span            int default 0 comment '隔开几周',
+   repeat_day           int default 1 comment '重复日',
+   primary key (id),
+   constraint FK_fk_repeat_roo foreign key (room_id)
+      references mrbs_room (id)
+)
+comment = "会议预定规则"
+ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+
+/*==============================================================*/
+/* Table: mrbs_schedule                                         */
+/*==============================================================*/
+create table mrbs_schedule
+(
+   id                   int(11) not null auto_increment,
+   start_time           datetime not null comment '开始时间',
+   end_time             datetime not null comment '结束时间',
+   room_id              int(11) not null comment '会议室',
+   create_by            varchar(80) not null default '' comment '添加者',
+   preside              varchar(80) not null default '' comment '预定者，预定者主持会议',
+   repeat_id            int not null comment '对应规则',
+   description          text not null comment '会议室描述',
+   num                  int not null default 0 comment '与会人数',
+   title                varchar(255),
+   primary key (id),
+   constraint FK_fk_schedule_repeat foreign key (repeat_id)
+      references mrbs_repeat (id) on delete restrict on update restrict,
+   constraint FK_fk_schedule_room foreign key (room_id)
+      references mrbs_room (id) on delete restrict on update restrict
+)
+ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+
+/*==============================================================*/
+/* Table: mrbs_schedule_user                                    */
+/*==============================================================*/
+create table mrbs_schedule_user
+(
+   userid               int,
+   scheduleid           int,
+   constraint FK_shedule_key foreign key (scheduleid)
+      references mrbs_schedule (id) on delete restrict on update restrict,
+   constraint FK_user_key foreign key (userid)
+      references mrbs_user (id) on delete restrict on update restrict
+)
+comment = "参加会议的用户";
+
---
+++ document/sql/testData.sql
@@ -0,0 +1,69 @@
+USE bmrbs;
+set names 'utf8';
+--对数据库中的外键加上索引，增强关联查询时候的效率
+create index INDEX_ROMM_AREAID on mrbs_room (area_id);
+create index INDEX_SCHEDULE_ROOMID on MRBS_SCHEDULE(room_id);
+
+insert into mrbs_authorities values(1,'AUTH_CHANGEAREANOTE','改变area公告');
+insert into mrbs_roles values(1,'行政人员');
+insert into mrbs_roles_authorities values(1,1);
+
+
+INSERT INTO mrbs_area VALUES ('1', '灵鹫宫', '8976','','');
+INSERT INTO mrbs_area VALUES ('2', '燕子坞', '5511/157032685','','');
+INSERT INTO mrbs_area VALUES ('3', '镜台小筑', '5743/hi:some','','');
+
+INSERT INTO mrbs_user
+   (id, login_name, name, email, tel, hi, department, view_choice, status, password)
+VALUES
+   (1, 'hongzhi', '张无忌', 'zhanghongzhi@baidu.com', '', '', '', '', 0, 'hongzhi');
+INSERT INTO mrbs_user
+   (id, login_name, name, email, tel, hi, department, view_choice, status, password)
+VALUES
+   (2, 'tiantian', '甜哥', 'tiange@some.com', '', '', '', '', 0, 'tiange');
+
+update mrbs_user set assistant=1 where login_name like '%hongzhi%';
+update mrbs_user set assistant=1 where login_name like '%tiantian%';
+
+insert into mrbs_users_roles values(1,(select id from mrbs_user where login_name like '%tiantian%'));
+insert into mrbs_users_roles values(1,(select id from mrbs_user where login_name like '%zhanghongzhi%'));
+
+INSERT INTO mrbs_room(id,area_id,room_name,room_admin_email,capacity,description) VALUES ('2', '1', '一枝春', '4356', '6', null);
+INSERT INTO mrbs_room(id,area_id,room_name,room_admin_email,capacity,description) VALUES ('3', '1', '双声子', '2345', '10', '');
+INSERT INTO mrbs_room(id,area_id,room_name,room_admin_email,capacity,description) VALUES ('4', '1', '三台令', '3234', '10', '');
+INSERT INTO mrbs_room(id,area_id,room_name,room_admin_email,capacity,description) VALUES ('5', '1', '四季花', '2345', '8', '');
+INSERT INTO mrbs_room(id,area_id,room_name,room_admin_email,capacity,description) VALUES ('6', '1', '五月天', '3234', '6', null);
+INSERT INTO mrbs_room(id,area_id,room_name,room_admin_email,capacity,description) VALUES ('7', '1', '六么序', '2355', '10', '');
+INSERT INTO mrbs_room(id,area_id,room_name,room_admin_email,capacity,description) VALUES ('8', '1', '七里香', '4567', '12', null);
+INSERT INTO mrbs_room(id,area_id,room_name,room_admin_email,capacity,description) VALUES ('9', '1', '八拍蛮', '4356', '8', '');
+INSERT INTO mrbs_room(id,area_id,room_name,room_admin_email,capacity,description) VALUES ('10', '1', '九回肠', '3456', '8', null);
+INSERT INTO mrbs_room(id,area_id,room_name,room_admin_email,capacity,description) VALUES ('11', '1', '十爱词', '4366', '8', null);
+INSERT INTO mrbs_room(id,area_id,room_name,room_admin_email,capacity,description) VALUES ('12', '1', '一江春水', '4356', '12', '');
+INSERT INTO mrbs_room(id,area_id,room_name,room_admin_email,capacity,description) VALUES ('13', '1', '二泉映月', '7654', '12', '');
+INSERT INTO mrbs_room(id,area_id,room_name,room_admin_email,capacity,description) VALUES ('14', '1', '梅花三弄', '4367', '8', '');
+INSERT INTO mrbs_room(id,area_id,room_name,room_admin_email,capacity,description) VALUES ('15', '1', 'yi', '3221/2356', '150', null);
+INSERT INTO mrbs_room(id,area_id,room_name,room_admin_email,capacity,description) VALUES ('16', '1', '八节同欢', '5419', '16', '');
+INSERT INTO mrbs_room(id,area_id,room_name,room_admin_email,capacity,description) VALUES ('85', '1', '九重春色', '5405', '10', '');
+
+INSERT INTO mrbs_room(id,area_id,room_name,room_admin_email,capacity,description) VALUES ('54', '3', '一寸金', '7101', '2', null);
+INSERT INTO mrbs_room(id,area_id,room_name,room_admin_email,capacity,description) VALUES ('55', '3', '双色莲', '7102', '10', null);
+INSERT INTO mrbs_room(id,area_id,room_name,room_admin_email,capacity,description) VALUES ('56', '3', '三登乐', '7103', '8', null);
+INSERT INTO mrbs_room(id,area_id,room_name,room_admin_email,capacity,description) VALUES ('57', '3', '四和春', '7104', '6', null);
+INSERT INTO mrbs_room(id,area_id,room_name,room_admin_email,capacity,description) VALUES ('58', '3', '五更转', '7105', '8', null);
+INSERT INTO mrbs_room(id,area_id,room_name,room_admin_email,capacity,description) VALUES ('59', '3', '六洲歌', '7106', '10', null);
+INSERT INTO mrbs_room(id,area_id,room_name,room_admin_email,capacity,description) VALUES ('60', '3', '齐天乐', '7107', '8', null);
+INSERT INTO mrbs_room(id,area_id,room_name,room_admin_email,capacity,description) VALUES ('61', '3', '八音谐', '7108', '6', null);
+INSERT INTO mrbs_room(id,area_id,room_name,room_admin_email,capacity,description) VALUES ('62', '3', '九张机', '7109', '8', null);
+INSERT INTO mrbs_room(id,area_id,room_name,room_admin_email,capacity,description) VALUES ('63', '3', '十拍子', '7110', '10', null);
+INSERT INTO mrbs_room(id,area_id,room_name,room_admin_email,capacity,description) VALUES ('64', '3', '万年欢', '7111', '16', null);
+
+INSERT INTO mrbs_room(id,area_id,room_name,room_admin_email,capacity,description) VALUES ('106', '2', '将进酒', '6115', '10', '');
+INSERT INTO mrbs_room(id,area_id,room_name,room_admin_email,capacity,description) VALUES ('107', '2', '水云游', '6112', '10', '');
+INSERT INTO mrbs_room(id,area_id,room_name,room_admin_email,capacity,description) VALUES ('102', '2', '一剪梅', '6110', '20', null);
+INSERT INTO mrbs_room(id,area_id,room_name,room_admin_email,capacity,description) VALUES ('103', '2', '西湖春', '6111', '20', null);
+INSERT INTO mrbs_room(id,area_id,room_name,room_admin_email,capacity,description) VALUES ('104', '2', '东湖月', '6118', '10', '');
+INSERT INTO mrbs_room(id,area_id,room_name,room_admin_email,capacity,description) VALUES ('105', '2', '瑶池宴', '6113', '6', null);
+
+
+
+
